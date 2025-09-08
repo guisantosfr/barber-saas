7b418054cc3a3f614972b1f5cebf9ef0057c7c88
Guilherme Santos <santosgui678@gmail.com>
2025-09-08 19:32:37 -0300

    feat: footer

--- app/page.tsx
+++ app/page.tsx
@@ -81,8 +81,16 @@ export default async function Home() {
             )
           }
         </div>
-
       </div>
+
+      <footer>
+        <Card className="py-6 px-5">
+          <CardContent>
+            <p className="text-sm text-gray-400">© { new Date().getFullYear() } Copyright <span className="font-bold">FSW Barber</span>.</p>
+
+          </CardContent>
+        </Card>
+      </footer>
     </>
   );
 }
