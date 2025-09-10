4defd81ea6fbb656ca4dc521a0c891a63bc7b2ff
Guilherme Santos <santosgui678@gmail.com>
2025-09-10 20:59:03 -0300

    refactor: sidebar component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { quickSearchoptions } from "../_constants/search";
-import { Avatar, AvatarImage } from "./ui/avatar";
-import Link from "next/link";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
 
 const Header = () => {
   return (
@@ -20,58 +18,7 @@ const Header = () => {
             </Button>
           </SheetTrigger>
 
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle className="text-left">Menu</SheetTitle>
-            </SheetHeader>
-
-            <div className="flex items-center py-5 border-b border-solid gap-3">
-              <Avatar>
-                <AvatarImage src="https://avatars.githubusercontent.com/u/32960040?v=4" />
-              </Avatar>
-
-              <div>
-                <p className="font-bold">Guilherme Santos</p>
-                <p className="text-sx">santosgui678@gmail.com</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2 py-5 border-b border-solid">
-              <SheetClose asChild>
-                <Button className="gap-2 justify-start" variant="ghost" asChild>
-                  <Link href="/">
-                    <HomeIcon size={18} />
-                    Início
-                  </Link>
-                </Button>
-              </SheetClose>
-
-              <Button className="gap-2 justify-start" variant="ghost">
-                <CalendarIcon size={18} />
-                Agendamentos
-              </Button>
-            </div>
-
-            <div className="flex flex-col gap-2 py-5 border-b border-solid">
-              {
-                quickSearchoptions.map(option => (
-                  <Button className="gap-2 justify-start" variant="ghost" key={option.title}>
-                    <Image alt={option.title} src={option.imageUrl} height={18} width={18} />
-                    {option.title}
-                  </Button>
-                ))}
-            </div>
-
-            <div className="flex flex-col gap-2 py-5 border-b border-solid">
-              <Button className="justify-start gap-2" variant="ghost">
-                <LogOutIcon size={18} />
-                Sair da conta
-              </Button>
-
-            </div>
-
-
-          </SheetContent>
+          <Sidebar />
         </Sheet>
       </CardContent>
     </Card>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,9 +1,12 @@
 import PhoneItem from "@/app/_components/phone-item";
 import ServiceItem from "@/app/_components/service-item";
+import Sidebar from "@/app/_components/sidebar";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet";
 import { prisma } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, Link, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface BarbershopPageProps {
@@ -39,9 +42,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
                     </Link>
                 </Button>
 
-                <Button size="icon" variant="secondary" className="absolute right-4 top-4">
-                    <MenuIcon />
-                </Button>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline" className="absolute top-4 right-4">
+                            <MenuIcon />
+                        </Button>
+                    </SheetTrigger>
+
+                    <Sidebar />
+                </Sheet>
             </div>
 
             <div className="p-5 border-b border-solid">
