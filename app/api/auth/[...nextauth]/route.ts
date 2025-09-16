4d24abfb507e47ec944b3515e317fc3a86072184
Guilherme Santos <santosgui678@gmail.com>
2025-09-16 17:14:31 -0300

    fix: remove timeout

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,10 +10,7 @@ const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
-            httpOptions : {
-                timeout: 10000
-            }
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string
         })
     ]
 })
